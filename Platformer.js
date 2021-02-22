41e3c0e1aaa5f978ff4259a3376be61ef3efd744
ericli3690 <ericli3690@gmail.com>
2021-02-22 15:32:27 -0700

    canvas works, but cannot resize during the game

--- Platformer.js
+++ Platformer.js
@@ -1,25 +1,16 @@
-console.log('Ta-Da!');
+console.log('Platformer');
 
 //defining the canvas and the pen
-var body = document.body;
 var game = document.getElementById('game');
 var ctx = game.getContext('2d');
+
+//making the game fit the window
+game.width = window.innerWidth;
+game.height = window.innerHeight;
+
 //what colour the pen is, the font
 ctx.fillStyle = 'black';
 ctx.font = "15px Arial";
-//drawing
-ctx.fillRect(50, 50, 100, 100);
-ctx.fillText('hi', 50, 50);
 
-var canvas = document.getElementById("game");
-var ctx = game.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
-
-function resizeCanvas() {
-  game.width = window.innerWidth;
-  game.height = window.innerHeight;
-  body.width = window.innerwidth;
-  body.height = window.innerHeight;
-}
+//testing
+ctx.fillText('Hello World', 50, 50);
