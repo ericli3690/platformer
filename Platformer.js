e9bffc2ab8addc01ec263a2ab159edc395686fc6
ericli3690 <ericli3690@gmail.com>
2021-02-22 14:44:53 -0700

    working on the canvas handover to kiana

--- Platformer.js
+++ Platformer.js
@@ -1,1 +1,25 @@
 console.log('Ta-Da!');
+
+//defining the canvas and the pen
+var body = document.body;
+var game = document.getElementById('game');
+var ctx = game.getContext('2d');
+//what colour the pen is, the font
+ctx.fillStyle = 'black';
+ctx.font = "15px Arial";
+//drawing
+ctx.fillRect(50, 50, 100, 100);
+ctx.fillText('hi', 50, 50);
+
+var canvas = document.getElementById("game");
+var ctx = game.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+function resizeCanvas() {
+  game.width = window.innerWidth;
+  game.height = window.innerHeight;
+  body.width = window.innerwidth;
+  body.height = window.innerHeight;
+}
