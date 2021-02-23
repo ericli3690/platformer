91d28bbbe85c6ee90a693a071e0c1e25bf931501
alpacapajamas <ahmadi.k@wcs-g.com>
2021-02-23 14:01:06 -0700

    Some broken gravity and notes

--- Platformer.js
+++ Platformer.js
@@ -10,10 +10,6 @@ var height = window.innerHeight;
 game.width = width;
 game.height = height;
 
-//what colour the pen is, the font
-ctx.fillStyle = 'black';
-ctx.font = "15px Arial";
-
 //when loading images
 function loadImages(mySrc, x, y, ctxi, firstTimeLoading) {
   var imageToDraw = new Image();
@@ -27,11 +23,12 @@ function loadImages(mySrc, x, y, ctxi, firstTimeLoading) {
   }
 }
 
+
 //character
 //looks, position variables
 var characterImage = './images/imagges.png';
-var characterX = 0;
-var characterY = 0;
+var characterX = 50;
+var characterY = 250;
 //drawing the character
 function drawCharacter(first) {
   ctx.clearRect(0, 0, width, height)
@@ -66,14 +63,14 @@ function keyUp(event) {
     dDown = false;
   }
 }
-
+//character speed
 function whileDown() {
   if ((aDown || dDown) == true) {
     if (aDown == true && characterX > 0) {
-      characterX -= 2;
+      characterX -= 0.5;
     }
     if (dDown == true && characterX < width - 50) {
-      characterX += 2;
+      characterX += 1;
     }
     drawCharacter(false);
   }
@@ -83,3 +80,58 @@ setInterval(whileDown, 1);
 
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
+
+
+//gravity
+
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 80, 75);
+    myGameArea.start();
+}
+//making another canvas? Need to link this to previous canvas
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+// all gravity and speed, i think this is good
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+    }
+}
+//identifiers again, need to link code with existing canvas
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
