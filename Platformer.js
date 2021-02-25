88d9ce0255ef748b8bdef103e75047f0d238a84a
ericli3690 <ericli3690@gmail.com>
2021-02-25 14:57:49 -0700

    leaving off for now error between canvas and player

--- Platformer.js
+++ Platformer.js
@@ -30,7 +30,7 @@ class Player {
 
   }
   draw(target, first) {
-    loadImage(sprite, x, y, target.ctx, first);
+    loadImage(this.sprite, this.x, this.y, target.ctx, first);
   }
 }
 
@@ -39,10 +39,13 @@ class Canvas {
     this.identity = identity;
   }
   ready() {
-    this.ctx = identity.getContext('2d');
+    this.ctx = this.identity.getContext('2d');
     this.identity.width = width;
     this.identity.height = height;
     this.interval = setInterval(this.update(), 20);
+  }
+  stop() {
+    clearInterval(this.interval);
   }
   clear() {
     this.ctx.clearRect(0, 0, width, height);
@@ -57,12 +60,21 @@ class Canvas {
 //first time drawing the character
 var characterImages = ['./images/imagges.png'];
 var characterStartingX = 50;
-var characterStartingY = 250;
+var characterStartingY = 235;
+var player;
+var playerCanvas;
+//canvases
+function startGame() {
+  playerCanvas = new Canvas(document.getElementById('player'));
+  backgroundCanvas = new Canvas(document.getElementById('background'));
+  foregroundCanvas = new Canvas(document.getElementById('foreground'));
+  //player canvas
+  player = new Player(characterStartingX, characterStartingY, characterImages[0]);
+  playerCanvas.ready();
+  player.draw(playerCanvas, true);
+}
 
-var player = new Player(characterStartingX, characterStartingY, characterImages[0]);
-var canvas = new Canvas(document.getElementById('game'));
-canvas.ready();
-player.draw(canvas, true);
+startGame();
 
 //keydown variables
 var aDown = false;
@@ -107,32 +119,7 @@ setInterval(whileDown, 1);
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
 
-
-//gravity
-
-var myGamePiece;
-
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 80, 75);
-    myGameArea.start();
-}
-//making another canvas? Need to link this to previous canvas
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    stop : function() {
-        clearInterval(this.interval);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
+/*
 // all gravity and speed, i think this is good
 function component(width, height, color, x, y, type) {
     this.type = type;
@@ -155,3 +142,16 @@ function component(width, height, color, x, y, type) {
         this.y += this.speedY + this.gravitySpeed;
     }
 }
+this.newPos = function() {
+    this.gravitySpeed += this.gravity;
+    this.x += this.speedX;
+    this.y += this.speedY + this.gravitySpeed;
+    this.hitBottom();
+  }
+  this.hitBottom = function() {
+    var rockbottom = myGameArea.canvas.height - this.height;
+    if (this.y > rockbottom) {
+      this.y = rockbottom;
+    }
+  }
+*/
